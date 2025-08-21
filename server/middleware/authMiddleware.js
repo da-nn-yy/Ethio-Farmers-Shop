247099f50bb7c7d28a051d05513f9c18c5718d07
da-nn-yy <db7394928@gmail.com>
2025-08-21 15:09:35 +0300

    Add authentication middleware and database configuration

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const admin = require('../config/firebase-admin');
+
+const verfifyToken = async (req, res, next) => {
+  const token = req.headers.authoriazation?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+  }
+};
+
+module.exports = verfifyToken;
