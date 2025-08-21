127ddaa3ebcce624bed4a7de5d776b0ad493c8a6
da-nn-yy <db7394928@gmail.com>
2025-08-21 15:09:13 +0300

    Add Firebase Admin SDK for authentication and database management

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
-// server/index.js
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Allows server to accept JSON in request bodies
+// EthioFarmers Middleware
+app.use(cors());
+app.use(express.json());
 
-// Basic test route to see if the server is working
+// Db connection
+
+app.get('/api/test-db', async (req,res) => {
+  try {
+    const [rows] = await db.execute('SELECT * FROM users');
+    res.json({message: "Database connection successful!", users: rows})
+  } catch (error) {
+    console.error('Database connection failed', error);
+    res.status(500).json({message: "Database connection failed!"});
+  }
+})
+
 app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from the Yabelat backend!' });
+  res.json({ message: 'Hello from Ethio Farmers backend!' });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
