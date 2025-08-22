406338c12c1050cc8dd2310bf8cd429b8a35aa84
da-nn-yy <db7394928@gmail.com>
2025-08-22 10:15:37 +0300

    Add lucide-react icons and implement React Router with authentication context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Navbar from "./component/Navbar.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
+
       <Navbar/>
-    </>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+      </Routes>
+    </Router>
+
   )
 }
 
