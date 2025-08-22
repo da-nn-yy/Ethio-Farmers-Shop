cb3ac7bb58d20de9a4934d33403ed658c7e3de50
da-nn-yy <db7394928@gmail.com>
2025-08-22 16:42:05 +0300

    Move AuthProvider from main.jsx to App.jsx for better component organization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./component/Navbar.jsx";
+import { AuthProvider } from './contexts/AuthContexts.jsx';
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
 
 function App() {
 
   return (
+    <AuthProvider>
     <Router>
 
       <Navbar/>
@@ -16,7 +18,7 @@ function App() {
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
-
+    </AuthProvider>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContexts.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-<AuthProvider>
+
   <StrictMode>
     <App />
   </StrictMode>,
-</AuthProvider>
 )
