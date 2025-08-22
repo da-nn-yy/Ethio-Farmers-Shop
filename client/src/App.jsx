3f8af5b8b9c22b52b05ff40fb67679d0a58fb21b
da-nn-yy <db7394928@gmail.com>
2025-08-22 11:05:26 +0300

    Add login and register routes to main application routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import Navbar from "./component/Navbar.jsx";
+import Login from "./pages/login.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,8 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<h1>Register Page</h1>} />
       </Routes>
     </Router>
 
