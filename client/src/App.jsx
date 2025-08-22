b96fc42c2d720189cab3e5bf5e3d0f8cd56329a6
da-nn-yy <db7394928@gmail.com>
2025-08-22 11:37:01 +0300

    Update routing to use Signup component and remove unused state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react'
 import Navbar from "./component/Navbar.jsx";
-import Login from "./pages/login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
@@ -13,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<h1>Register Page</h1>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
 
