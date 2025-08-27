a2a50c6516252d44e00b3fdbef5a99cb6e26efc7
da-nn-yy <db7394928@gmail.com>
2025-08-27 12:55:02 +0300

    Remove client folder

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerUser, login } = require('../controllers/authController');
-const { verifyToken } = require("../middleware/authMiddleware");
-
-router.post("/register", verifyToken, registerUser);
-router.post("/login", verifyToken, login);
-
-module.exports = router;
