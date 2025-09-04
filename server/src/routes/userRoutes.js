b48818dc1ba47f05f9232060b58aba7f60dc32c1
da-nn-yy <db7394928@gmail.com>
2025-09-04 22:25:09 +0300

    Refactor user routes to use cleaner URL structure and centralized authentication middleware

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { authGuard } from "../middleware/auth.js";
-import { upsertUser, getMe, updateMe, uploadMyAvatar } from "../controllers/userController.js";
+import {
+  upsertUser,
+  getMe,
+  updateMe,
+  uploadMyAvatar
+} from "../controllers/userController.js";
 import upload, { handleUploadError } from "../middleware/upload.js";
 
 const router = Router();
 
-router.post('/users', authGuard, upsertUser);
-router.get('/users/me', authGuard, getMe);
-router.put('/users/me', authGuard, updateMe);
-router.post('/users/me/avatar', authGuard, upload.single('image'), handleUploadError, uploadMyAvatar);
+// All routes require authentication
+router.use(authGuard);
+
+// User management
+router.post('/', upsertUser);
+router.get('/me', getMe);
+router.put('/me', updateMe);
+router.post('/me/avatar', upload.single('image'), handleUploadError, uploadMyAvatar);
 
 export default router;
